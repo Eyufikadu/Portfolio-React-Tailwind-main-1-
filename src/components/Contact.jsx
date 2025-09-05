3ac8ec740d8b06d133f0a407e14590aa8ddf84a9
eyu <eyufikadu652@gmil.com>
2025-09-05 09:18:41 +0300

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
             </div>
             <div className='mb-4'>
                 <FaMapMarkedAlt className='inline-block text-green-400 mr-2'></FaMapMarkedAlt>
-                <span>sreet, city, pronice, country</span>
+                <span>Hawassa, Ethiopia</span>
             </div>
           </div>
           <div className='flex-1 w-full'>
